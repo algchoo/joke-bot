851b0968c6cbbea8e3772e28e93df66383a9f682
algchoo <algchoo@gmail.com>
2019-11-10 13:22:28 -0500

    Started code for API Requests

---
+++ src/main/java/com/APIRequest.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.net.HttpURLConnection;
+
+public class APIRequest {
+
+//    It will have a type (GET, POST)
+//    It will have a destination. Fortunately, this will always be the same because containers dude
+//    There will be a payload or response from the API
+//    Use HttpUrlConnection
+
+    private String destinationURL;
+
+    public String getJoke() { return null; }
+
+    public String listJokes(String type) { return null; }
+
+}
